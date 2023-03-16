ea1eb362a586b715971c28ea4d99c43013bfe9f5
an tran <thanhan-tran@am-bition.vn>
2023-03-16 16:41:08 +0700

    add firebase

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,7 +3,12 @@ import { initializeFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  //
+  apiKey: 'AIzaSyA4D6mUpt98kdKtdGy0DqHoY2W09KQI46Y',
+  authDomain: 'chat-app-f401b.firebaseapp.com',
+  projectId: 'chat-app-f401b',
+  storageBucket: 'chat-app-f401b.appspot.com',
+  messagingSenderId: '1042363916345',
+  appId: '1:1042363916345:web:28355cda3e23fce360ede8',
 };
 
 const app = initializeApp(firebaseConfig);
